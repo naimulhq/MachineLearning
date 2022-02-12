7cca2d4b8b8f4607a9878fac06b68d54785dcbee
naimulhq <naimul@ucsb.edu>
2022-02-12 03:38:50 -0800

    Naimul - Added function for parsing CSV data

--- DecisionTrees/TitanicDecisionTree.py
+++ DecisionTrees/TitanicDecisionTree.py
@@ -8,9 +8,23 @@ def read_csv_data(csv_file):
         csv_file_reader = csv.reader(titanic_csv)
         for row in csv_file_reader:
             titanic_data.append(row)
+    return titanic_data
 
+def parse_csv_data(data):
+    parsed_data = []
+    total_columns = len(data[0])
+
+    for column in range(total_columns):
+        parsed_data.append([])
+    
+    for row in data:
+        for column in range(len(row)):
+            parsed_data[column].append(row[column])
+    
+    return parsed_data
 
 
 if __name__ == '__main__':
     train_data = read_csv_data(CSV_TRAIN_FILE_LOCATION)
-    
+    parsed_train_data = parse_csv_data(train_data)
+    del train_data
